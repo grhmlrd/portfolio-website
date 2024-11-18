00126cfbeb5e4dfcbe143636d822aafb3c4e36f7
joshua-estallo <179905741+grhmlrd@users.noreply.github.com>
2024-11-18 19:56:42 +0800

    add back-to-top functionality

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+// start: toggle-theme functionality
+
 const toggle_theme = document.getElementById("toggle-theme");
 const body = document.querySelector("body");
 toggle_theme.addEventListener("click", () => {
-	body.classList.toggle("dark-theme");
+  body.classList.toggle("dark-theme");
 });
+
+// end: toggle-theme functionality
+
+// start: back to top functionality
+let back_to_top = document.getElementById("back-to-top");
+
+back_to_top.addEventListener("click", () => {
+  topFunction();
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    back_to_top.style.display = "block";
+  } else {
+    back_to_top.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// end: back to top functionality
